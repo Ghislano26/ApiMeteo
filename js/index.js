2f1a64d9ed418b0dc3c6ee7970436d6592d207ff
Ghislano26 <ghislainatebachouacha@gmail.com>
2023-12-02 14:18:32 +0100

    version mobile 2.0

--- js/index.js
+++ js/index.js
@@ -1,15 +1,35 @@
-async function fetchApp() {
-  const app = await fetch("text.json");
-  if (app.ok === true) {
-    return app.json().then((data) => {
-      console.log(data);
-      document.getElementById("ville").innerHTML = data.tmp;
-    });
-  }
-  throw new Error("impossible d'acceder au serveur");
-}
-fetchApp().then((res) => console.log(res));
+const myApiKey = "8c28d7ff09fe18e0d7726382f4431c04";
 
+async function fetchApp(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}&units=metric&lang=fr`;
+  const app = await fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("ville").innerHTML = data.name;
+      document.getElementById("tmp").innerHTML =
+        "<ion-icon name='thermometer-outline'></ion-icon> " +
+        data.main.temp +
+        " °";
+      document.getElementById("humid").innerHTML =
+        "<ion-icon name='water'></ion-icon> " + data.main.humidity + " %";
+      document.getElementById("vent").innerHTML =
+        "<ion-icon name='rainy-sharp'></ion-icon>" + data.wind.speed + " Km/H";
+    })
+    .catch((err) => console.log("Erreur :" + err));
+}
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let ville = document.getElementById("city").value;
+  fetchApp(ville);
+});
+
+// Appel par defaut
+fetchApp("Yaounde");
+
+// throw new Error("impossible d'acceder au serveur");
 // async function fetchUser() {
 //   let app = await fetch("text.json");
 //   if (app.ok === true) {
