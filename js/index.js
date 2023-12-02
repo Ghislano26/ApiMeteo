52cc74b999c2833508a9b806a08d4c55a73f6f84
Ghislano26 <ghislainatebachouacha@gmail.com>
2023-12-02 10:38:00 +0100

    version mobile 1.0

---
+++ js/index.js
@@ -0,0 +1,21 @@
+async function fetchApp() {
+  const app = await fetch("text.json");
+  if (app.ok === true) {
+    return app.json().then((data) => {
+      console.log(data);
+      document.getElementById("ville").innerHTML = data.id;
+    });
+  }
+  throw new Error("impossible d'acceder au serveur");
+}
+fetchApp().then((res) => console.log(res));
+
+// async function fetchUser() {
+//   let app = await fetch("text.json");
+//   if (app.ok === true) {
+//     return app.text();
+//   }
+//   throw new Error("Impossible d'acceder au serveur. Verifiez votre url");
+// }
+
+// fetchUser().then(res => console.log(res))
